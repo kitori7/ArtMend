2306703ba52545d2e5791c2050eccee3d1804a42
qwj <2841897438@qq.com>
2024-06-01 21:50:24 +0800

    添加我的头部

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { View } from "@tarojs/components";
-import { Button } from "@nutui/nutui-react-taro";
+import { Button, NavBar } from "@nutui/nutui-react-taro";
+import { More, ArrowLeft } from '@nutui/icons-react'
 import "./index.scss";
 
 function Index() {
   return (
-    <View className="nutui-react-demo">
-      <View className="index">
-        <Button type="primary" className="btn">
-          我的页面
-        </Button>
-      </View>
+    <View className="mine">
+      <NavBar
+      >
+        我的
+      </NavBar>
     </View>
   );
 }
